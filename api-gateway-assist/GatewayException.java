57cde023c85318efc0e50787c4d9c10c57b2ef73
doununu <2338752958@qq.com>
2025-07-08 19:34:44 +0800

    Add files via upload

---
+++ api-gateway-assist/GatewayException.java
@@ -0,0 +1,12 @@
+package cn.bugstack.gateway.assist;
+
+public class GatewayException extends RuntimeException {
+
+    public GatewayException(String msg) {
+        super(msg);
+    }
+
+    public GatewayException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
